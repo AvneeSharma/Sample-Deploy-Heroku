c75d2522acf1d9c2899323aef8662ed4bfe57ed1
Avnee Sharma <113236130+AvneeSharma@users.noreply.github.com>
2023-06-22 00:03:01 +0530

    Update and rename index.js to server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+const PORT = process.env.PORT || 4444
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+app.listen(PORT, (req, res) => {
+    console.log('Server started at http://localhost:${PORT}')
+})
